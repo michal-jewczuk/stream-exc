908ebc28fd84a8f2716cdae1ccb78c7f3d09d11a
michal-jewczuk <michal.jewczuk@outlook.com>
2021-06-04 10:12:54 +0200

    Create assignment 7 and 8

---
+++ src/main/java/test/java/assignment/AssignmentEight.java
@@ -0,0 +1,13 @@
+package test.java.assignment;
+
+
+/**
+ * Assignment
+ * <p>
+ * Print number of times a letter 't' or 'T' appears in names.
+ * <p>
+ * Do not forget to add test.java.assignment to Executor!
+ */
+public class AssignmentEight {
+
+}
---
+++ src/main/java/test/java/assignment/AssignmentSeven.java
@@ -0,0 +1,13 @@
+package test.java.assignment;
+
+
+/**
+ * Assignment
+ * <p>
+ * Print second lowest product of month and year
+ * <p>
+ * Do not forget to add test.java.assignment to Executor!
+ */
+public class AssignmentSeven {
+
+}
